f70977f50efd22b03a282fdde3d40f55ab12b852
Ian Pires de Campos <ianp259@gmail.com>
2025-06-04 15:17:01 -0300

    minor bug corrections

--- datastructures.py
+++ datastructures.py
@@ -32,6 +32,12 @@ class LinkedList:
         else:
             new_node.set_next_node(self.head)
             self.head = new_node
+        
+    def get_head_value(self):
+        if self.head is None:
+            return None
+        
+        return self.get_head_node().get_value()
 
 
 class Queue:
@@ -74,7 +80,7 @@ class HashMap:
         self.array_size = array_size
         self.array = [None for i in range(array_size)]
 
-    def my_hash(self, key, count_collisions):
+    def my_hash(self, key, count_collisions=0):
         key_bytes = key.encode()
         hash_code = sum(key_bytes)
 
@@ -89,11 +95,13 @@ class HashMap:
         array_index = self.compressor(self.my_hash(key))
         current_array_value_at_idx = self.array[array_index]
 
-        if current_array_value_at_idx == None:
+        if current_array_value_at_idx is None:
             self.array[array_index] = [key, value]
+            return
 
         if current_array_value_at_idx[0] == key:
             self.array[array_index] = [key, value]
+            return
 
         number_of_collisions = 1
 
@@ -105,6 +113,7 @@ class HashMap:
 
             if current_array_value_at_idx is None or current_array_value_at_idx[0] == key:
                 self.array[new_idx] = [key, value]
+                return
 
             number_of_collisions += 1
         return
@@ -126,15 +135,14 @@ class HashMap:
 
             new_hash_code = self.my_hash(key, number_of_collisions)
             new_idx = self.compressor(new_hash_code)
-            possible_return = self.array = new_idx
+            possible_return = self.array[new_idx]
 
             if possible_return is None:
                 return None
 
             if possible_return[0] == key:
                 return possible_return[1]
+            
+            number_of_collisions += 1
 
         return
-
-
-
